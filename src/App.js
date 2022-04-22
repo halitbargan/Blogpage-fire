780b5d31923a78bcde1202d6e886cf6150669b69
halitbargan <hltbrgn@gmail.com>
2022-04-22 04:37:30 +0300

    login.js added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
 import './App.css';
 import Navbar from './components/Navbar';
+import Login from "../src/pages/Login"
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Login/>
     </div>
   );
 }
